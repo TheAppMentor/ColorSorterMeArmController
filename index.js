f71ac8da21d369fee3b32ccc3a2527bfbd75e06e
Prashanth <chapparfellow@gmail.com>
2018-11-10 18:42:55 +0000

    Got the servo position for Jaw opening

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Gpio = require('pigpio').Gpio;
 const Promise = require('bluebird')
 
-const servoArmForBack = new Gpio(10, {mode: Gpio.OUTPUT});
-const servoJaw = new Gpio(14, {mode: Gpio.OUTPUT});
+//const servoArmForBack = new Gpio(10, {mode: Gpio.OUTPUT});
+const servoJaw = new Gpio(10, {mode: Gpio.OUTPUT});
 
 
 let jawFullOpenPos = 800
-let jawFullClosePos = 1700
+let jawFullClosePos = 1600
 
 let armFullForward = 2500
 let armFullRetract = 1800
@@ -19,16 +19,46 @@ let armFullRotateRight = 1500
 
 let increment = 50;
 
-function openJaw(percent = 1){
-    let finalPusleWidth = jawFullOpenPos * percent 
+closeJaw(1)
+//openJaw(1)
 
-    setInterval(() => {
+var jawServoPos = jawFullClosePos 
 
-        if (finalPulseWidth >= jawFullOpenPos){
-            return 
+
+function closeJaw(percent = 1){
+    console.log("IN Close JAwe function")   
+    var finalPulseWidth = jawFullClosePos * percent 
+
+    var closeJawLoop = setInterval(() => {
+
+        console.log("In Set interval")
+        if (jawServoPos >= jawFullClosePos){
+        console.log("Returning ")
+          clearInterval(closeJawLoop) 
         }
 
-        jawFullOpenPos.servoWrite(pulseWidth);
-        pulseWidth += increment;
+        servoJaw.servoWrite(jawServoPos);
+        jawServoPos += increment;
+    
+    }, 150);
+}
+
+function openJaw(percent = 1){
+    
+    console.log("IN Open Jaw function")   
+
+    var finalPulseWidth = jawFullOpenPos * percent 
+
+    var openJawLoop = setInterval(() => {
+
+        console.log("In Set interval")
+        if (jawServoPos <= jawFullOpenPos){
+        console.log("Returning ")
+          clearInterval(openJawLoop) 
+        }
+
+        servoJaw.servoWrite(jawServoPos);
+        jawServoPos -= increment;
+    
     }, 150);
 }
