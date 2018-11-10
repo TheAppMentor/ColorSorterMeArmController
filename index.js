367209a69291ea5a8c55954969401489b9675d93
Prashanth <chapparfellow@gmail.com>
2018-11-10 15:02:59 +0000

    Got the servo position for Jaw opening

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const five = require('johnny-five')
+const Gpio = require('pigpio').Gpio;
+ 
+const motor = new Gpio(10, {mode: Gpio.OUTPUT});
 
-var board = new five.Board();
 
-board.on("ready", function() {
-
-    var servo = new five.Servo(10);
-
-    // Sweep from 0-180 and repeat.
-    servo.sweep();
-});
+let jawFullOpenPos = 800
+let jawFullClosePos = 1700
 
 
 
+
+
+
+let pulseWidth = 800;
+//let increment = 100;
+let increment = 50;
+
+//  motor.servoWrite(11000);
+  //motor.servoWrite(11000);
+  //motor.servoWrite(11000);
+
+setInterval(() => {
+    console.log("Pulse Width : " + pulseWidth)
+    motor.servoWrite(pulseWidth);
+ 
+  pulseWidth += increment;
+
+  if (pulseWidth >= 1600) {
+      increment = -100;
+    } else if (pulseWidth <= 800) {
+        increment = 50;
+    }
+}, 150);
+
+// Pulse Width = 11000 ms => 90 deg Right
+// Pulse Width < 11000  ms => 0 deg 
+// Pulse Width > 11000 ms => 180 deg Right
