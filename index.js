b9f4c403207d2f23b182749d9ed600701ac6f074
Prashanth Moorthy <chapparfellow@gmail.com>
2018-11-10 18:02:30 +0530

    Some changes..

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const five = require('johnny-five')
 
-// Create a Servo on PIN # 14
-new five.Servo(14);
+var board = new five.Board();
 
+board.on("ready", function() {
+
+    var servo = new five.Servo(10);
+
+    // Sweep from 0-180 and repeat.
+    servo.sweep();
+});
 
 
 
