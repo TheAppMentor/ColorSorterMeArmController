ed13f162a707f740298da758e882fe201aaf20ca
Prashanth Moorthy <chapparfellow@gmail.com>
2018-11-14 22:44:34 +0530

    adding a Switch to control the pi

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const button = new Gpio(18, {
  
  
 // Level must be stable for 10 ms before an alert event is emitted.
-button.glitchFilter(100);
+button.glitchFilter(10000);
  
 button.on('alert', (level, tick) => {
     console.log("IS RUNNING : " + isRunning)
